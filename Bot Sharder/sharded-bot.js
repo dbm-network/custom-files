848b58f50117fa0fc4950180210c3ee9efc0bf40
General Wrex <1731139+generalwrex@users.noreply.github.com>
2018-09-06 14:08:22 -0700

    Fixed: test.js to bot.js

--- Bot Sharder/sharded-bot.js
+++ Bot Sharder/sharded-bot.js
@@ -104,7 +104,7 @@ if(!getToken()){
 }
 
 const Discord = require('discord.js');
-const manager = new Discord.ShardingManager('./test.js', {
+const manager = new Discord.ShardingManager('./bot.js', {
 	token: getToken() 
 	});
 	
