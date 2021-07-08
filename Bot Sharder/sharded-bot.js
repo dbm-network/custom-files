01d6be3804ccdf6c0ce11d9f33a07f1dbc628374
TheMonDon <11539895+TheMonDon@users.noreply.github.com>
2021-07-07 20:21:10 -0500

    Update Bot Sharder/sharded-bot.js
    Co-authored-by: Almeida <almeidx@pm.me>

--- Bot Sharder/sharded-bot.js
+++ Bot Sharder/sharded-bot.js
@@ -93,5 +93,4 @@ const manager = new ShardingManager('./bot.js', {
 // Emitted when a shard is created
 manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id} launched`));
 
-// Spawn your shards
 manager.spawn();
