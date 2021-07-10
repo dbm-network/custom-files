df516136b19a45dc152966d509575961f2eebd99
almeidx <almeidx@pm.me>
2021-07-10 21:48:22 +0100

    updated the eslint config to match the mods one

--- Bot Sharder/README.md
+++ Bot Sharder/README.md
@@ -1,38 +1,33 @@
+**NOTE: Do not use this unless your bot is in over 2000 guilds, there is no point if its below, it uses more resources, and will probably break commands that you already use**
 
-**NOTE: Do not use this unless your bot is in over 2000 guilds, there is no point if its below, it uses more resources, and will probally break commands that you already use**
+# DBM Bot Sharding
 
-# DBM Mods Bot Sharding
+Allows your bot created by Discord Bot Maker to take advantage of sharding!
 
-**Allows your bot created by Discord Bot Maker to take advantage of sharding!**
+## Installation
 
-
-Installation
-====
-Download the [ZIP file](https://downgit.github.io/#/home?url=https://github.com/dbm-network/custom-files/blob/master/Bot%20Sharder/sharded-bot.js) and extract the file to your bot folder.
+Download the [ZIP file] and extract the file to your bot folder.
 
 Your Bot folder should look like this:
 
 ![shard](https://i.imgur.com/sHqbJjV.png)
 
+## Running
 
-Running
-====
+Open a Command Prompt window on the bot folder and run `node sharded-bot.js`. It should look somewhat similar to this:
 
-Once those are in the folder, up at the top where the path is, select everything in there and type cmd and press enter:
-![cmd](https://i.imgur.com/XWKxwob.png)
-
-
-
-In the Command Prompt window that opens, type `node sharded-bot.js`. It should look somewhat similar to this.
 ![node](https://i.imgur.com/AKuzOrR.png)
 
+If you want to provide a different shard count, add `--shard_count=[number]` after `node sharded-bot.js`; ex. `node sharded-bot.js --shard_count=3`.
+The default `shard_count` flag is set to `auto` (determined by discord.js), which is fine for most bots.
 
-If you want to provide a different shard count add `--shard_count=[number]` after `node sharded-bot.js`; ex. `node sharded-bot.js --shard_count=3`.
-The default `shard_count` parameter is set to `auto` (determined by discord.js), which is fine for most bots.
+To change the bot startup file, add `--startup=./index.js` after `node sharded-bot.js`; ex. `node sharded-bot.js --startup=./index.js`.
+The default `startup` parameter is set to `bot.js`.
 
-To change the bot startup file add `--startup=./index.js` after `node sharded-bot.js`; ex. `node sharded-bot.js --startup=./index.js`.
-The default `startup` parameter is set to `bot.js` (Default file for Discord Bot Maker)
+**If you want to do anything across shards you will need to use** [`client.shard.broadcastEval()`]
 
-**If you want to do anything across shards you will need to use** [`client.shard.broadCastEval()`](https://discord.js.org/#/docs/main/stable/class/ShardClientUtil?scrollTo=broadcastEval)
+For more information in regards to sharding, check [this guide].
 
-**Please read the API docs!**
+[zip file]: https://downgit.github.io/#/home?url=https://github.com/dbm-network/custom-files/blob/master/Bot%20Sharder/sharded-bot.js
+[this guide]: https://discordjs.guide/sharding/
+[`client.shard.broadcasteval()`]: https://discord.js.org/#/docs/main/stable/class/ShardClientUtil?scrollTo=broadcastEval
--- Bot Sharder/sharded-bot.js
+++ Bot Sharder/sharded-bot.js
@@ -5,37 +5,37 @@ const version = '1.1';
 // Include discord.js and original check
 const { version: djsVersion, ShardingManager } = require('discord.js');
 if (djsVersion < '12.0.0') {
-  console.log('This version of Discord Bot Maker requires Discord.JS v12.\nPlease use "Project > Module Manager" and "Project > Reinstall Node Modules" to update to Discord.JS v12.');
+  console.log(
+    'This version of Discord Bot Maker requires Discord.JS v12.\nPlease use "Project > Module Manager" and "Project > Reinstall Node Modules" to update to Discord.JS v12.',
+  );
   throw new Error('Need Discord.JS v12 to Run!!!');
 }
 
 console.log('-'.repeat(50));
-console.log('TheMonDon\'s DBM Bot Sharder');
+console.log("TheMonDon's DBM Bot Sharder");
 console.log(`Version: ${version}`);
-console.log('You can change the amount of shards by providing \'--shard_count=[number]\' (default: auto)');
-console.log('You can change the bot file by providing \'--startup=./index.js\' (default bot.js)');
+console.log("You can change the amount of shards by providing '--shard_count=[number]' (default: auto)");
+console.log("You can change the bot file by providing '--startup=./index.js' (default bot.js)");
 console.log('-'.repeat(50));
 
 let totalShards = 'auto';
 let startup = './bot.js';
 
-function getArgs () {
+function getArgs() {
   const args = {};
-  process.argv
-    .slice(2, process.argv.length)
-    .forEach(arg => {
-      if (arg.slice(0, 2) === '--') {
-        const longArg = arg.split('=');
-        const longArgFlag = longArg[0].slice(2, longArg[0].length);
-        const longArgValue = longArg.length > 1 ? longArg[1] : true;
-        args[longArgFlag] = longArgValue;
-      } else if (arg[0] === '-') {
-        const flags = arg.slice(1, arg.length).split('');
-        flags.forEach(flag => {
-          args[flag] = true;
-        });
-      }
-    });
+  process.argv.slice(2, process.argv.length).forEach((arg) => {
+    if (arg.slice(0, 2) === '--') {
+      const longArg = arg.split('=');
+      const longArgFlag = longArg[0].slice(2, longArg[0].length);
+      const longArgValue = longArg.length > 1 ? longArg[1] : true;
+      args[longArgFlag] = longArgValue;
+    } else if (arg[0] === '-') {
+      const flags = arg.slice(1, arg.length).split('');
+      flags.forEach((flag) => {
+        args[flag] = true;
+      });
+    }
+  });
   return args;
 }
 
@@ -51,14 +51,14 @@ if (args && args.startup) {
 
 console.log(`Starting the DBM Bot with ${totalShards} total shards...`);
 
-// dbm's encryption system
+// dbms' encryption system
 const crypto = require('crypto');
 let password = '';
 let token;
 
 try {
   password = require('discord-bot-maker');
-} catch { }
+} catch {}
 
 const decrypt = (text) => {
   if (password.length === 0) return text;
@@ -68,30 +68,31 @@ const decrypt = (text) => {
   return dec;
 };
 
-const fs = require('fs');
-const path = require('path');
-const filePath = path.join(process.cwd(), 'data', 'settings.json');
+const { existsSync, readFileSync } = require('fs');
+const { join } = require('path');
+const filePath = join(process.cwd(), 'data', 'settings.json');
 
-if (fs.existsSync(filePath)) {
-  let content = fs.readFileSync(filePath);
+if (existsSync(filePath)) {
+  const content = readFileSync(filePath).toString();
   try {
-    if (typeof content !== 'string' && content.toString) content = content.toString();
     token = JSON.parse(decrypt(content)).token;
-  } catch {
-    console.error('There was issue parsing settings.json!');
+  } catch (err) {
+    console.error('There was issue parsing settings.json! ', err.stack || err);
   }
 } else {
   console.error('Could not find the settings.json file');
 }
 
-if (!token) console.error('Token must be supplied in \'settings.json\' in the data folder, double check your bot settings!');
+if (!token) {
+  console.error("Token must be supplied in 'settings.json' in the data folder, double check your bot settings!");
+}
 
 // Create your ShardingManger instance
 const manager = new ShardingManager(startup, {
   // for ShardingManager options see:
-  // https://discord.js.org/#/docs/main/v12/class/ShardingManager
+  // https://discord.js.org/#/docs/main/stable/class/ShardingManager
   totalShards,
-  token
+  token,
 });
 
 manager.on('shardCreate', (shard) => console.log(`Shard ${shard.id} launched`));
